59ae335823db45914b968e8ef0b0b07bf8dadb21
ksarch-store <ksarch-store@baidu.com>
2014-01-12 19:10:29 +0800

    change carbon port, fix boot scripts

--- bin/horizon.d
+++ bin/horizon.d
@@ -19,7 +19,7 @@ start () {
 
 stop () {
     # TODO: write a real kill script
-    ps aux | grep 'horizon-agent.py start' | grep -v grep | awk '{print $2 }' | xargs kill -9
+    ps aux | grep 'horizon-agent.py' | grep -v grep | awk '{print $2 }' | xargs kill -9
     /usr/bin/env python $BASEDIR/src/horizon/horizon-agent.py stop
         RETVAL=$?
         if [[ $RETVAL -eq 0 ]]; then
